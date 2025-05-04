0f30ce717b9a6dc00ec7ddd082d92fd4fb281587
Porygon61 <126583112+Porygon61@users.noreply.github.com>
2025-05-04 23:56:14 +0200

    GiveHead.java aktualisieren

--- src/main/java/com/anon/perfectUnityPlugin/Heads/GiveHead.java
+++ src/main/java/com/anon/perfectUnityPlugin/Heads/GiveHead.java
@@ -6,13 +6,13 @@ import org.bukkit.entity.Player;
 public class GiveHead {
     private final perfectUnityPlugin plugin = perfectUnityPlugin.getInstance();
 
-    public static void giveSingleHead(Player player, String headName) {
+    public static void giveSingleHead(Player player, String headName, YamlConfiguration playerData) {
         //validation
 
         //give-logic
     }
 
-    public static void openAmountGUI(Player player, String headName) {
+    public static void openAmountGUI(Player player, String headName, YamlConfiguration playerData) {
         //validation
 
         //gui logic
